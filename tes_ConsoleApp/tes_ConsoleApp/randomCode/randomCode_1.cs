8366dc20f32c8a9e7cb589bcbcae742ecc5503d9
Dl <765560184@qq.com>
2019-08-21 16:50:13 +0800

    唯一摩尔斯密码词

--- tes_ConsoleApp/tes_ConsoleApp/randomCode/randomCode_1.cs
+++ tes_ConsoleApp/tes_ConsoleApp/randomCode/randomCode_1.cs
@@ -14,7 +14,9 @@ namespace tes_ConsoleApp.randomCode
         public RandomCode_1()
         {
             //NumJewelsInStones_init();
-            ToLowerCase_init();
+            //ToLowerCase_init();
+
+            //UniqueMorseRepresentations_init();
         }
 
         private void NumJewelsInStones_init()
@@ -31,7 +33,7 @@ namespace tes_ConsoleApp.randomCode
         /// <param name="J"></param>
         /// <param name="S"></param>
         /// <returns></returns>
-        public int NumJewelsInStones(string J, string S)
+        private int NumJewelsInStones(string J, string S)
         {
             /*给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。
              * S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
@@ -64,7 +66,7 @@ namespace tes_ConsoleApp.randomCode
         /// </summary>
         /// <param name="str"></param>
         /// <returns></returns>
-        public string ToLowerCase(string str)
+        private string ToLowerCase(string str)
         {
             char[] cha = new char[str.Length];
             for (byte i = 0; i < str.Length; i++)
@@ -79,6 +81,45 @@ namespace tes_ConsoleApp.randomCode
                 }
             }
             return new string(cha);
+        }
+
+        private void  UniqueMorseRepresentations_init()
+        {
+            string[] words = new string[] { "gin", "zen", "gig", "msg" };
+
+            Console.WriteLine(UniqueMorseRepresentations(words));
+        }
+
+        /// <summary>
+        /// 唯一摩尔斯密码词
+        /// </summary>
+        /// <param name="words"></param>
+        /// <returns></returns>
+        private int UniqueMorseRepresentations(string[] words)
+        {
+            string[] hashMoscode = new string[] { ".-","-...","-.-.","-..",
+                ".","..-.","--.","....","..",".---","-.-",".-..","--","-.",
+                "---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-",
+                "-.--","--.."};
+            
+            string scode;
+            HashSet<string> recordMoscode = new HashSet<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                scode = "";
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    scode += hashMoscode[ words[i][j] - 97];
+                }
+
+                if (!recordMoscode.Contains(scode))
+                {
+                    recordMoscode.Add(scode);
+                }
+            }
+            
+
+            return recordMoscode.Count;
         }
     }
 }
